bd81b4ea9140c36029aa93db3d4e264558105326
nicomu99 <nicolaimu1212@gmail.com>
2024-11-22 10:12:25 +0100

    Implemented collision detection

--- src/model/Circle.cpp
+++ src/model/Circle.cpp
@@ -11,7 +11,19 @@ Circle::Circle(): GameObject(0, 0), radius(0) { }
 Circle::Circle(double x, double y): GameObject(x, y), radius(10) { }
 
 bool Circle::checkGridCollision(const Grid& grid) const {
-    return true;
+    int min_x = Grid::toTileCoordinate(center.x - radius);
+    int min_y = Grid::toTileCoordinate(center.y - radius);
+    int max_x = Grid::toTileCoordinate(center.x + radius);
+    int max_y = Grid::toTileCoordinate(center.y + radius);
+    for(int i = min_x; i <= max_x; i++) {
+        for(int j = min_y; j <= max_y; j++) {
+            const Tile& target_tile = grid.getTile(i, j);
+            if(target_tile.isNonWalkable()) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 void Circle::initialize(Position, int) { }
